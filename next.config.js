30c86b5340e124ca01481f894add64d2afe656b2
Fredy <163409338+Dev-Fredy@users.noreply.github.com>
2025-04-29 21:13:27 +0000

    V 1.0.0 : Updated webworke

--- next.config.js
+++ next.config.js
@@ -3,8 +3,5 @@ module.exports = {
   experimental: {
     clientTraceMetadata: false,
   },
-  webpack(config) {
-    config.resolve.alias['webworker-threads'] = require.resolve('./mocks/webworker-threads.js');
-    return config;
-  },
+  
 };
